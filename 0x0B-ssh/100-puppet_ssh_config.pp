1ae41770ec8f32cec7c65b2a3b0d09e7b385015f
ritchie254 <richardoduor012@gmail.com>
2024-03-24 04:25:15 -0700

    ssh is fun

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# connect to a server Execute a command
+
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config':
+	        path    => '/bin/'
+}
