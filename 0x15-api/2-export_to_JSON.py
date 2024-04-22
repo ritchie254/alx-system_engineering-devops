b08df20359e78cb32ef8ee07bb313e30e6d006fb
ritchie254 <richardoduor012@gmail.com>
2024-04-22 13:02:08 -0700

    api

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script to fetch api information
+"""
+import requests
+import sys
+
+api = "https://jsonplaceholder.typicode.com"
+"""api url"""
+
+
+if __name__ == "__main__":
+    all_list = []
+    if len(sys.argv) > 1:
+        id = int(sys.argv[1])
+        req = requests.get("{}/todos/".format(api))
+        user = requests.get("{}/users/{}".format(api, id)).json()
+        todos = list(filter(lambda x: x.get('userId') == id, req.json()))
+        file_name = "{}.json".format(id)
+        with open(file_name, "w") as file:
+            for i in todos:
+                file.write(
+                        '"{}","{}","{}","{}"\n'.format(
+                            id,
+                            user.get("username"),
+                            i.get("completed"),
+                            i.get("title")
+                            )
+                        )
